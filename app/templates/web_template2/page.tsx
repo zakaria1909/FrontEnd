c80766b985b5fb52d2d53c58a6c83e287c8e2e5d
Zakaria Ahmada <zakariaahmada@gmail.com>
2025-10-20 13:54:39 +0700

    commit

--- app/templates/web_template2/page.tsx
+++ app/templates/web_template2/page.tsx
@@ -1,22 +1,8 @@
-"use client";
-import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
-import Education from "./components/Education/Education";
-import Experience from "./components/Experience/Experience";
-import Project from "./components/Project/Project";
-import Footer from "./components/Footer/Footer";
-
-export default function Page({user}: {user: any}) {
+export default function Template2({ user }: { user: any }) {
   return (
-    <>
-      <Navbar user={user} />
-      <Hero user={user} />
-      <main>
-        <Education user={user} />
-        <Experience user={user} />
-        <Project user={user} />
-      </main>
-      <Footer user={user} />
-    </>
+    <div>
+      <h1>{user.name}</h1>
+      <p>{user.bio}</p>
+    </div>
   );
 }
